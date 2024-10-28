0a7796e7336a8c1d18c74159f074ff98555a81ff
mahar <mahar@RahulMaha>
2024-10-28 22:57:13 +0530

    i need to commit

---
+++ C2TC_MAHALAKSHMI_R/src/com/tns/dayone/firstprogram.java
@@ -0,0 +1,12 @@
+package com.tns.dayone;
+
+public class firstprogram {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	       System.out.println("Hello world");
+		}
+
+	}
+
+
