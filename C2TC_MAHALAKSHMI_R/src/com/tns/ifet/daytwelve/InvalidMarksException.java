ae7d1ad05755382cd55a2493e9b6f9151f808119
mahar <mahar@RahulMaha>
2024-11-13 18:39:49 +0530

    I need to commit

---
+++ C2TC_MAHALAKSHMI_R/src/com/tns/ifet/daytwelve/InvalidMarksException.java
@@ -0,0 +1,19 @@
+package com.tns.ifet.daytwelve;
+	public class InvalidMarksException extends Exception{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public InvalidMarksException() {
+			super();
+		}
+
+		public InvalidMarksException(String message) {
+			super(message);
+		}
+
+		
+	}
+
