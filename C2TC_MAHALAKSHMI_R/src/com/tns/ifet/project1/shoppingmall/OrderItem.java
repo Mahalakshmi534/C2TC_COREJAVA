081a9a162972d5de25e65644349788835e1d9743
mahar <mahar@RahulMaha>
2025-02-12 09:57:41 +0530

    I need to commit

---
+++ C2TC_MAHALAKSHMI_R/src/com/tns/ifet/project1/shoppingmall/OrderItem.java
@@ -0,0 +1,8 @@
+package com.tns.ifet.project1.shoppingmall;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jak
+
